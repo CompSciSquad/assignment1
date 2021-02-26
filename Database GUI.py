ed5194ea3a97fbe59e0056e558017d3a6997ed51
Chloe-A <73178383+Chloe-A@users.noreply.github.com>
2021-02-26 21:23:18 +0200

    Update Database GUI.py

--- Database GUI.py
+++ Database GUI.py
@@ -3,14 +3,17 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import *
 
+# connect to database
 my_conn = sqlite3.connect('Asset Tracking.s3db')
 
+# display window
 my_w = tk.Tk()
 my_w.geometry("1150x300")
 
 
+# function to display data from database on window
 def display_data():
-    r_set = my_conn.execute('''SELECT * from User''')
+    r_set = my_conn.execute('''SELECT * from User''')  # select all from the User table
     i = 1  # row value inside the loop
     for user in r_set:
         for j in range(len(user)):
@@ -28,9 +31,8 @@ def display_data():
     e = Label(my_w, width=10, text='Asset', borderwidth=2, relief='ridge', anchor='w', bg='grey', fg='white')
     e.grid(row=0, column=3)
 
-    i = 1
 
-
+# function to add data into database
 def add_data():
     try:
         my_id = tid2.get('1.0', END)  # read name
@@ -41,44 +43,46 @@ def add_data():
             my_conn.execute("""
             INSERT INTO User(ID, Name, Surname, Asset)
             VALUES (?,?,?,?)
-            """, (my_id, my_name, my_surname, my_asset))
+            """, (my_id, my_name, my_surname, my_asset))  # SQL to insert user details into database 
             my_conn.commit()
             display_data()
             options.set("")  # reset the option menu
             tid2.delete('1.0', END)  # reset the text entry box
             tName.delete('1.0', END)  # reset the text entry box
             tsname.delete('1.0', END)  # reset the text entry box
-        else:
+        else: # if the input was invalid, throw error
             messagebox.showerror("Error", "Invalid input")
-    except:
+    except: # if the input was invalid, throw error
         messagebox.showerror("Error", "Invalid input")
 
 
+# function to search for the specified user 
 def search(id):
     try:
         int(id)  # check input is integer or not
         try:
-            q = "SELECT * FROM User WHERE ID= "+id
+            q = "SELECT * FROM User WHERE ID= "+id  # select all from the table which have the specified ID
             my_cursor = my_conn.execute(q)
             data_row = my_cursor.fetchone()
             my_str.set(data_row)
-        except sqlite3.Error as my_error:
+        except sqlite3.Error as my_error:  # if there was an error with the database, throw error
             print("error: ", my_error)
-    except:
+    except:  # if there was invalid input, throw error
         my_str.set("Check input")
 
 
+# function that counts down to terminate the program
 def countdown(time):
-    if time == -1:
+    if time == -1:  # if the time reaches -1, terminate the program
         my_w.destroy()
     else:
-        l4 = tk.Label(my_w, text="time remaining: %d seconds" % time)
+        l4 = tk.Label(my_w, text="time remaining: %d seconds" % time)  # show with a label how much time is left
         l4.grid(row=50, column=51)
 
-        my_w.after(1000, countdown, time-1)
+        my_w.after(1000, countdown, time-1)  # every 1000 ms countdown by 1 each time
 
 
-display_data()
+display_data()  # display table with table data
 
 # add employee label
 lSearch = tk.Label(my_w,  text='Search Employee', font=('Helvetica', 12), width=30, anchor="c")
@@ -102,7 +106,7 @@ my_str = tk.StringVar()
 l2 = tk.Label(my_w,  textvariable=my_str, width=30, fg='red')
 l2.grid(row=3, column=10, columnspan=2)
 
-my_str.set("Output")
+my_str.set("Output")  # set the label by default with this string value
 
 # add employee label
 lEmp = tk.Label(my_w,  text='Add Employee', font=('Helvetica', 12), width=30, anchor="c")
@@ -151,6 +155,6 @@ bAdd.grid(row=7, column=7)
 bExit = tk.Button(my_w, text='Exit', width=8, bg='light grey', command=my_w.destroy)
 bExit.grid(row=50, column=50)
 
-countdown(500)
+countdown(500)  # call countdown function from 500 s
 
 my_w.mainloop()
